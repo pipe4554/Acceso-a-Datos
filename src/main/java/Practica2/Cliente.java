3ff42041fcc9ea98fcfba440664751d7e1f50868
pipe4554 <0273lmejia@e-itaca.es>
2020-12-17 19:18:21 +0100

    Cambios de variables en clientes y mas contenido en main

---
+++ src/main/java/Practica2/Cliente.java
@@ -0,0 +1,157 @@
+package Practica2;
+
+public class Cliente {
+	private int codigo_cliente;
+	private String nombre;
+	private String nombre_contacto;
+	private String apellido_contacto;
+	private String telefono;
+	private String fax;
+	private String linea_direccion1;
+	private String linea_direccion2;
+	private String ciudad;
+	private String region;
+	private String pais;
+	private String codigo_postal;
+	private int codigo_empleado_rep_ventas;
+	private double limite_credito;
+	
+	public Cliente() {
+		//constructor vacio para implementar los clientes
+	}
+
+	/*public Cliente(int codigo_cliente, String nombre, String nombre_contacto, String apellido_contacto, String telefono,
+			String fax, String linea_direccion1, String linea_direccion2, String ciudad, String region, String pais,
+			String codigo_postal, int codigo_empleado_rep_ventas, double limite_credito) {
+		super();
+		this.codigo_cliente = codigo_cliente;
+		this.nombre = nombre;
+		this.nombre_contacto = nombre_contacto;
+		this.apellido_contacto = apellido_contacto;
+		this.telefono = telefono;
+		this.fax = fax;
+		this.linea_direccion1 = linea_direccion1;
+		this.linea_direccion2 = linea_direccion2;
+		this.ciudad = ciudad;
+		this.region = region;
+		this.pais = pais;
+		this.codigo_postal = codigo_postal;
+		this.codigo_empleado_rep_ventas = codigo_empleado_rep_ventas;
+		this.limite_credito = limite_credito;
+	}*/
+
+	public int getCodigo_cliente() {
+		return codigo_cliente;
+	}
+
+	public void setCodigo_cliente(int codigo_cliente) {
+		this.codigo_cliente = codigo_cliente;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getNombre_contacto() {
+		return nombre_contacto;
+	}
+
+	public void setNombre_contacto(String nombre_contacto) {
+		this.nombre_contacto = nombre_contacto;
+	}
+
+	public String getApellido_contacto() {
+		return apellido_contacto;
+	}
+
+	public void setApellido_contacto(String apellido_contacto) {
+		this.apellido_contacto = apellido_contacto;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getFax() {
+		return fax;
+	}
+
+	public void setFax(String fax) {
+		this.fax = fax;
+	}
+
+	public String getLinea_direccion1() {
+		return linea_direccion1;
+	}
+
+	public void setLinea_direccion1(String linea_direccion1) {
+		this.linea_direccion1 = linea_direccion1;
+	}
+
+	public String getLinea_direccion2() {
+		return linea_direccion2;
+	}
+
+	public void setLinea_direccion2(String linea_direccion2) {
+		this.linea_direccion2 = linea_direccion2;
+	}
+
+	public String getCiudad() {
+		return ciudad;
+	}
+
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+
+	public String getRegion() {
+		return region;
+	}
+
+	public void setRegion(String region) {
+		this.region = region;
+	}
+
+	public String getPais() {
+		return pais;
+	}
+
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+
+	public String getCodigo_postal() {
+		return codigo_postal;
+	}
+
+	public void setCodigo_postal(String codigo_postal) {
+		this.codigo_postal = codigo_postal;
+	}
+
+	public int getCodigo_empleado_rep_ventas() {
+		return codigo_empleado_rep_ventas;
+	}
+
+	public void setCodigo_empleado_rep_ventas(int codigo_empleado_rep_ventas) {
+		this.codigo_empleado_rep_ventas = codigo_empleado_rep_ventas;
+	}
+
+	public double getLimite_credito() {
+		return limite_credito;
+	}
+
+	public void setLimite_credito(double limite_credito) {
+		this.limite_credito = limite_credito;
+	}
+	
+	
+	
+}
--- src/main/java/Practica2/Main.java
+++ src/main/java/Practica2/Main.java
@@ -1,9 +1,12 @@
 package Practica2;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+
+	public static Scanner kb = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		Connection connection = null;
@@ -17,6 +20,7 @@ public class Main {
 			Statement statement = null;
 			try {
 				statement = connection.createStatement();
+				System.out.println("statement");
 
 			} catch (SQLException e) {
 				System.out.println("La conexion esta cerrada");
@@ -25,16 +29,17 @@ public class Main {
 
 			try {
 				ResultSet resultSet = statement.executeQuery("select * from empleado");
+				System.out.println("Result set");
 
 				while (resultSet.next()) {
 					String codigoEmpleado = resultSet.getString("codigo_empleado");
 					String nombre = resultSet.getString("nombre");
 				}
+				Menu();
 				connection.close();
 			} catch (Exception e) {
 				// TODO: handle exception
 			}
-			
 
 		}
 
@@ -49,49 +54,109 @@ public class Main {
 
 	}
 
-	public void Menu() {
+	public static void Menu() {
 		int opcion = 0;
-		Scanner kb = new Scanner(System.in);
+		System.out.println(
+				"Introduzca la opción deseada:\n 1.Añadir un cliente. \n 2.Mostrar un cliente. \n 3.Mostrar todos los clientes."
+						+ "\n 4.Buscar un cliente. \n 5.Editar un productor.");
+		opcion = kb.nextInt();
+
 		while (opcion != 0) {
 			try {
 				opcion = kb.nextInt();
 				switch (opcion) {
-				case 1:addClient();break;
-				case 2:showClient();break;
-				case 3:showAllClients();break;
-				case 4:searchClient();break;
-				case 5:editProduct();break;
+				case 1:
+					addClient();
+					break;
+				case 2:
+					showClient();
+					break;
+				case 3:
+					showAllClients();
+					break;
+				case 4:
+					searchClient();
+					break;
+				case 5:
+					editProduct();
+					break;
 
-				default:break;
+				default:
+					System.out.println("El valor no coincide con las opciones dadas");
+					break;
 				}
-				
+
 			} catch (Exception e) {
 				System.out.println("Has introducido un valor incorrecto");
 			}
 		}
 
 	}
-	
-	public void addClient() {
-		//añadir un cliente
-		System.out.println("Introduzca los datos del cliente");
+
+	public static void addClient() {
+		// Añadir un cliente
+
+		System.out.println("Introduzca los datos del cliente:");
+
+		Cliente client = new Cliente();
+		boolean confirmacion = false;
 		
+		while (confirmacion = true) {
+			try {
+				System.out.println("Introduzca el Código de cliente");
+				client.setCodigo_cliente(kb.nextInt());
+				System.out.println("Introduzca el nombre de cliente");
+				client.setNombre(kb.nextLine());
+				System.out.println("Introduzca el nombre de contacto");
+				client.setNombre_contacto(kb.nextLine());
+				System.out.println("Introduzca el apellido de contacto");
+				client.setApellido_contacto(kb.nextLine());
+				System.out.println("Introduzca el telefono");
+				client.setTelefono(kb.nextLine());
+				System.out.println("Introduzca el numero de fax");
+				client.setFax(kb.nextLine());
+				System.out.println("Introduzca el la dirección 1");
+				client.setLinea_direccion1(kb.nextLine());
+				System.out.println("Introduzca el la dirección 2");
+				client.setLinea_direccion2(kb.nextLine());
+				System.out.println("Introduzca la ciudad");
+				client.setCiudad(kb.nextLine());
+				System.out.println("Introduzca la región");
+				client.setRegion(kb.nextLine());
+				System.out.println("Introduzca el país");
+				client.setPais(kb.nextLine());
+				System.out.println("Introduzca el código postal");
+				client.setCodigo_postal(kb.nextLine());
+				System.out.println("Introduzca el código de empleado representante de ventas");
+				client.setCodigo_empleado_rep_ventas(kb.nextInt());
+				System.out.println("Introduzca el límite de crédito");
+				client.setLimite_credito(kb.nextDouble());
+
+				System.out.println(client.toString());
+				
+			} catch (Exception e) {
+				System.out.println("Has introducido un valor erroneo, introduce todos los datos de nuevo");
+			}
+		}
+
+		
+
 	}
-	
-	public void showClient() {
-		//mostrar un cliente determinado
+
+	public static void showClient() {
+		// mostrar un cliente determinado
 	}
-	
-	public void showAllClients() {
-		//mostrar todos los clientes de la lista clientes
+
+	public static void showAllClients() {
+		// mostrar todos los clientes de la lista clientes
 	}
-	
-	public void searchClient() {
-		//buscar un cliente de la tabla clientes
+
+	public static void searchClient() {
+		// buscar un cliente de la tabla clientes
 	}
-	
-	public void editProduct() {
-		//Editar un producto 
+
+	public static void editProduct() {
+		// Editar un producto
 	}
 
 }
